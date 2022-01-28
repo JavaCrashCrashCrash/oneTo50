a1f36bdf5b32d0f155282c278afca875622a1c49
jang6129 <jang6129@naver.com>
2022-01-28 18:55:16 +0900

    play update

--- src/oneTo50/controller/GameController.java
+++ src/oneTo50/controller/GameController.java
@@ -8,37 +8,39 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class GameController implements MouseListener {
-    UImodel uiModel;
-    MyPanel mp;
-    public GameController(UImodel uiModel, MyPanel mp) {
-        this.uiModel = uiModel;
-        this.mp = mp;
-    }
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        System.out.println("clicked");
-        Rect rect = uiModel.whoAmI(e.getX(), e.getY());
-        uiModel.compare(rect);
-        mp.repaint();
-    }
+	UImodel uiModel;
+	MyPanel mp;
 
-    @Override
-    public void mousePressed(MouseEvent e) {
+	public GameController(UImodel uiModel, MyPanel mp) {
+		this.uiModel = uiModel;
+		this.mp = mp;
+	}
 
-    }
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		Rect rect = uiModel.whoAmI(e.getX(), e.getY());
+		System.out.println(e.getX() + " " + e.getY());
+		uiModel.compare(rect);
+		mp.repaint();
+	}
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
+	@Override
+	public void mousePressed(MouseEvent e) {
 
-    }
+	}
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
+	@Override
+	public void mouseReleased(MouseEvent e) {
 
-    }
+	}
 
-    @Override
-    public void mouseExited(MouseEvent e) {
+	@Override
+	public void mouseEntered(MouseEvent e) {
 
-    }
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
 }
--- src/oneTo50/model/UImodel.java
+++ src/oneTo50/model/UImodel.java
@@ -4,62 +4,79 @@ import java.awt.*;
 import java.util.Random;
 
 public class UImodel {
-    int currentNumber = 1;
-    private Rect[] rects = new Rect[25];
-    public Rect[] getRects() {
-        return rects;
-    }
+	int currentNumber = 1;
+	boolean[] usedNumber = new boolean[25];
+	private Rect[] rects = new Rect[25];
 
-    // set Poistion for rects
-    public void setRects() {
-        int i = 0;
-        for (int y = 0; y < 5; y++) {
-            for (int x = 0; x < 5; x++) {
-                rects[i] = new Rect();
-                rects[i].size = 50;
-                rects[i].x = 70 + x * rects[i].size;
-                rects[i].y = 70 + y * rects[i].size;
-                rects[i].num = i + 1;
-                rects[i].color = Color.WHITE;
-                rects[i].back = 26 + i;
-                i += 1;
-            }
-        }
-    }
+	public Rect[] getRects() {
+		return rects;
+	}
 
-    // Creation random numbers
-    // and set numbers
+	// set Poistion for rects
+	public void setRects() {
+		int i = 0;
+		for (int y = 0; y < 5; y++) {
+			for (int x = 0; x < 5; x++) {
+				rects[i] = new Rect();
+				rects[i].size = 50;
+				rects[i].x = 70 + x * rects[i].size;
+				rects[i].y = 70 + y * rects[i].size;
+				rects[i].num = i + 1;
+				rects[i].color = Color.WHITE;
+				rects[i].back = 26 + i;
+				i += 1;
+			}
+		}
+	}
 
-    public void numShuffle() {
-        Random ran = new Random();
-        for (int i = 0; i < 1000; i++) {
-            int r = ran.nextInt(25);
-            int temp = rects[r].num;
-            rects[r].num = rects[0].num;
-            rects[0].num = temp;
+	// Creation random numbers
+	// and set numbers
 
-            r = ran.nextInt(25);
-            temp = rects[r].back;
-            rects[r].back = rects[0].back;
-            rects[0].back = temp;
-        }
-    }
+	public void numShuffle() {
+		Random ran = new Random();
+		for (int i = 0; i < 1000; i++) {
+			int r = ran.nextInt(25);
+			int temp = rects[r].num;
+			rects[r].num = rects[0].num;
+			rects[0].num = temp;
 
-    public Rect whoAmI(int x, int y) {
-        // Find rect
-        return null;
-    }
+			r = ran.nextInt(25);
+			temp = rects[r].back;
+			rects[r].back = rects[0].back;
+			rects[0].back = temp;
+		}
+	}
 
-    public void compare(Rect rect) {
+	public Rect whoAmI(int x, int y) {
+		// Find rect
+		Rect rect = new Rect();
+		for (Rect r : rects) {
+			if (x >= r.x && x < r.x + 50 && y >= r.y && y < r.y + 50) {
+				rect = r;
+			}
+		}
+		System.out.println(rect.num);
+		return rect;
+	}
 
-//        if (rect.num == currentNumber) {
-//            // refresh view
-//            currentNumber ++;
-//            // creation random number and set number
-//
-//        } else {
-//            // ignore
-//        }
-    }
+	public void compare(Rect rect) {
+		if (rect.num == currentNumber) {
+			// refresh view
+			currentNumber++;
+			// creation random number and set number
+			Random random = new Random();
+			while (true) {
+				int rectNumber = random.nextInt(24) + 26;
+				if (usedNumber[rectNumber - 26] == false) {
+					rect.num = rectNumber;
+					usedNumber[rectNumber - 26] = true;
+					break;
+				}
+			}
+		} else {
+			// ignore
+			System.out.println("Wrong Click");
+		}
+	}
 
 }
